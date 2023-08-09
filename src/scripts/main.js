31257d40895bd410794910fc33f30e5d79ae3613
joaoleandro <new.joaoleandro@gmail.com>
2023-08-09 18:09:39 -0300

    add video, adjustments made in the modal

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,16 @@
 const buttonOpenModal = document.querySelector(".button-open-modal")
 const modal = document.querySelector(".modal")
 const buttonModalClose = document.querySelector(".modal-close")
+const videoControls = document.querySelector(".video-play-gear-five")
 
 buttonOpenModal.addEventListener("click", () => {
     modal.style.display = 'block'
 })
 
 buttonModalClose.addEventListener("click", () => {
+    videoControls.pause()
     modal.style.display = 'none'
 })
+
+
+
